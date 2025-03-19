8681fd6bd41728808d6e24dff522f7cf48029774
Pititorial <pititorial@gmail.com>
2025-03-19 17:57:07 +0700

    ลบความสัมพันธ์และฟังก์ชันการแชร์โพสต์ออกจากโมเดล Post เนื่องจากการลบ Share model และ SharesController ในการปรับปรุงก่อนหน้า

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,19 +3,13 @@ class Post < ApplicationRecord
   
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :shares, dependent: :destroy
   
   has_many :liking_users, through: :likes, source: :user
-  has_many :sharing_users, through: :shares, source: :user
   
   validates :title, presence: true
   validates :content, presence: true
   
   def like_count
     likes.count
-  end
-  
-  def share_count
-    shares.count
   end
 end 
